4f97f3151e61d4cf872570117810a21e8682638c
hadesTso <caohades1996@outlook.com>
2020-05-06 17:13:13 +0800

    修改创角邮件

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -16,10 +16,12 @@ class ManagerController extends Controller
         $name    = $request->input('name');
         $remark  = $request->input('remark');
         $status  = $request->input('status');
+        $menu    = $request->input('menuList', null);
 
         $manager->manager_name = $name;
-        $manager->remark = $remark;
-        $manager->status = $status;
+        $manager->remark       = $remark;
+        $manager->status       = $status;
+        $manager->menu         = json_encode($menu);
         $res = $manager->save();
 
         if ($res) {
@@ -48,53 +50,32 @@ class ManagerController extends Controller
         $name    = $request->input('name');
         $remark  = $request->input('remark');
         $status  = $request->input('status');
-        $game_id = $request->input('game');
         $menu    = $request->input('menuList', null);
 
-        DB::beginTransaction();
-        try{
-            $manager->where(['id' => $id])
-                ->update([
-                    'manager_name'  => $name,
-                    'remark'        => $remark,
-                    'status'        => $status,
-                    'updated_at'    => date('Y-m-d H:i:s', time()),
-                ]);
 
-            ManagerMenu::where(['manager_id' => $id, 'game_id' => $game_id])->delete();
+        $result = $manager->where(['id' => $id])
+            ->update([
+                'manager_name'  => $name,
+                'remark'        => $remark,
+                'status'        => $status,
+                'menu'          => json_encode($menu),
+                'updated_at'    => date('Y-m-d H:i:s', time()),
+            ]);
 
-            if ($menu && $game_id) {
-                $menuArray = array();
-                foreach ($menu as $value) {
-                    $valueBase = json_decode($value, true);
-                    $menuArray[] = $valueBase['id'];
-                    if (isset($valueBase['children'])) {
-                        foreach ($valueBase['children'] as $val) {
-                            $menuArray[] = $val['id'];
-                            if (isset($val['children'])) {
-                                foreach ($val['children'] as $v) {
-                                    $menuArray[] = $v['id'];
-                                }
-                            }
-                        }
-                    }
-                }
-
-                foreach ($menuArray as $value) {
-                    ManagerMenu::create(['manager_id' => $id, 'menu_id' => $value, 'game_id' => $game_id]);
-                }
-            }
-            DB::commit();
+        if ($result) {
             return response(Response::Success('修改成功'));
-        } catch (\Exception $exception) {
-            DB::rollBack();
-            return response(Response::Error(trans("修改失败"), 400));
+        } else {
+            return response(Response::Error(trans('修改失败'), 400));
         }
     }
 
     public function list(Manager $managerModel)
     {
-        $list = $managerModel->select('id', 'manager_name', 'remark', 'status', 'created_at')->paginate(10);
+        $list = $managerModel->select('id', 'manager_name', 'remark', 'status', 'menu', 'created_at')->paginate(10);
+
+        foreach ($list as $value) {
+            $value['menu'] = json_decode($value['menu'], true);
+        }
 
         return response(Response::Success($list));
     }
